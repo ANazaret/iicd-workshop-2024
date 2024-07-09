ff2c11a0955d7c8c90e00658653ef2347ccd6c1b
Achille Nazaret <aon2108@columbia.edu>
2024-07-09 18:18:44 -0400

    restructure code

--- iicd_workshop_2024/gene_model.py
+++ iicd_workshop_2024/gene_model.py
@@ -1,12 +1,12 @@
 import abc
 
 import numpy as np
-import scipy
 import seaborn as sns
 import torch
 import torch.distributions as dist
-import tqdm
 from matplotlib import pyplot as plt
+
+from iicd_workshop_2024.inference import fit
 
 
 class BaseGeneModel(abc.ABC, torch.nn.Module):
@@ -133,22 +133,7 @@ class BaseGeneModel(abc.ABC, torch.nn.Module):
         lr: float
             Learning rate.
         """
-        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
-        data_X = adata.X
-        # check if sparse
-        if isinstance(data_X, scipy.sparse.csr_matrix):
-            data_X = data_X.toarray()
-        data_loader = torch.utils.data.DataLoader(data_X, batch_size=batch_size, shuffle=True)
-        pbar = tqdm.tqdm(total=epochs * len(data_loader))
-        for _ in range(epochs):
-            for x in data_loader:
-                optimizer.zero_grad()
-                loss = self.loss(x).mean()
-                loss.backward()
-                optimizer.step()
-                pbar.set_postfix(loss=loss.item())
-                pbar.update()
-        pbar.close()
+        fit(self, adata, epochs=epochs, batch_size=batch_size, lr=lr)
 
 
 def plot_gene_distribution(model: BaseGeneModel, adata, genes, n_cols=3):
